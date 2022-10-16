b7bd512e49ddd0ea16d7f343b266468a080f1edb
benfujiir <benfujiir@gmail.com>
2022-10-16 10:24:12 -0700

    Updated 5_15.py to output correct answer

--- chapter5/5_15.py
+++ chapter5/5_15.py
@@ -1,7 +1,8 @@
 n = cube = 0
 
-while cube < 12000:
-    cube = n ** 3
+while n ** 3 < 12000:
     n += 1
 
-print("Largest n value that is less than 1200 is:", round(cube ** (1/3)))
+n -= 1
+
+print("Largest n value that is less than 1200 is:", n)
